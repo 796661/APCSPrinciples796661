0bba43b453ddb882b8cb2a6ea084cc6ed9da146d
796661 <54591261+796661@users.noreply.github.com>
2019-10-09 08:50:57 -0700

    setup for program

---
+++ Lab1007 Bubble Sort/bubble.js.js
@@ -0,0 +1,11 @@
+//  Duncan Vaughan
+// 	10/4/19
+var list = [6, 8, 1, 5, 0, 7, 4, 2, 3];
+
+//  The draw function is called @ 30 fps
+function swap(list,a,b){
+  var temp = list[a];
+  list[a] = list[b];
+  list[b] = list[temp];
+}
+for(var )
