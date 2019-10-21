bebd67857a35e21cbd1f4935f2cdb5498569277a
796661 <796661@seq.org>
2019-10-21 08:31:25 -0700

    made insertion sort

--- Lab 1018 Insertion Sort/sketch.js
+++ Lab 1018 Insertion Sort/sketch.js
@@ -1,17 +1,30 @@
 //  Duncan Vaughan
 // 	10/4/19
-var list = [6, 8, 1, 5, 0, 7, 4, 2, 3];
+var list = [];
 
-//  The draw function is called @ 30 fps
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(235);
+  for (var i = 0; i < 10; i++){
+    list[i]= int(random(10));
+  }
+}
 function swap(list,a,b){
   var temp = list[a];
   list[a] = list[b];
   list[b] = list[temp];
 }
-for(var i = 1; i < arr.length; i++){
-  for(var j = i ; j > 0; j--){
-    if(arr[j] < arr[j-1]){
-      
+function insertionsort(){
+  for(var i=1; i<list.length; i++){
+    for(var j=i; j>0; j--){
+      if(list[j] < list[j-1]){
+        swap(list,j,j-1);
+      }
     }
   }
+  console.log(list);
+}
+function draw(){
+  insertionsort();
 }
