92a04719833c3d569a1a2ed67a094ac7e92bc241
796661 <796661@seq.org>
2019-11-04 08:49:07 -0800

    removed checkerboard for now, tried to make snake move

--- Lab 1031 Snake Game/Snake.js
+++ Lab 1031 Snake Game/Snake.js
@@ -2,7 +2,8 @@
 class Snake{
   constructor(x, y, dx, dy, w, id){
     this.loc=createVector(x,y);
-    this.vel=createVector(dx,dy)
+    this.vel.x = 0;
+    this.vel.y = 0;
     this.w=25;
     this.clr=color(255,0,0);
     this.id=id;
@@ -12,9 +13,32 @@ class Snake{
     this.update();
   }
   render(){
-
+    background(2, 2, 2);
+    fill(this.clr);
+    rect(this.loc.x,this.loc.y,this.w,this.w);
   }
+
   update(){
-    
+    if(keyIsPressed&&
+      keyCode === RIGHT_ARROW){
+        this.vel.x = 3;
+        this.vel.y = 0;
+      }
+    if(keyIsPressed&&
+      keyCode === LEFT_ARROW){
+        this.vel.x = -3;
+        this.vel.y = 0;
+        }
+    if(keyIsPressed&&
+      keyCode === UP_ARROW){
+        this.vel.x = 0;
+        this.vel.y = -3;
+      }
+    if(keyIsPressed&&
+      keyCode === DOWN_ARROW){
+        this.vel.x = 0;
+        this.vel.y = 3;
+        }
+    //this.loc.add(this.vel);
   }
 }
--- Lab 1031 Snake Game/sketch.js
+++ Lab 1031 Snake Game/sketch.js
@@ -4,37 +4,28 @@
 var squares = [];
 var segments = [];
 var food = 0;
+hitFood = 'no';
 var score = 0;
 function setup() {
   // put setup code here
   var cnv = createCanvas(800,800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);//background color
-  loadSquares();
-  for(var i = 0; i < squares.length; i++){
-    squares[i].render();
-  }
+  loadSnake();
 }
-function loadSquares(){
-  for (var i = 0; i < 32; i++){
-  for(var x = 0; x < 32; x++){
-    if(i%2===0){
-      if(x%2===0){
-        var shade=color(255, 255, 255);
-      }
-      if(x%2!==0){
-        var shade=color(255, 255, 255);
-      }
-    }
-    if(i%2!==0){
-      if(x%2!==0){
-        var shade=color(255, 255, 255);
-      }
-      if(x%2===0){
-        var shade=color(255, 255, 255);
-      }
-    }
-    squares[x+(32*i)]= new Square(25*x, 25*i, 25, 25, shade);
+
+function draw(){
+  runSnake();
+}
+function loadSnake(){
+  segments[0] = new Snake(0,0,50,50,0);
+}
+function runSnake(){
+  for (var i=0; i<segments.length; i++){
+    segments[i].run();
+    if (hitFood==='yes'){
+      segments[i+1]= new Snake(segments[i].loc.x-segments[i].w,segments[i].loc.y,50,50,i+1);
+      hitFood='no';
     }
   }
 }
