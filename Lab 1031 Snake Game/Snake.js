c703eadbf860fe1f43535bb26d5505db70f05d7e
796661 <796661@seq.org>
2019-11-12 08:50:57 -0800

    got rid of errors, but can't figure out how to load the snake

--- Lab 1031 Snake Game/Snake.js
+++ Lab 1031 Snake Game/Snake.js
@@ -1,44 +1,25 @@
 
 class Snake{
-  constructor(x, y, dx, dy, w, id){
-    this.loc=createVector(x,y);
-    this.vel.x = 0;
-    this.vel.y = 0;
-    this.w=25;
-    this.clr=color(255,0,0);
-    this.id=id;
+  constructor(x, y, dx, dy, w, c){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.w = w;
+    this.clr = color(random(0,255), random(0,255), random(0,255));
+    this.body = [];
   }
   run(){
     this.render();
-    this.update();
+  //  this.update();
   }
   render(){
-    background(2, 2, 2);
     fill(this.clr);
-    rect(this.loc.x,this.loc.y,this.w,this.w);
-  }
-
-  update(){
-    if(keyIsPressed&&
-      keyCode === RIGHT_ARROW){
-        this.vel.x = 3;
-        this.vel.y = 0;
-      }
-    if(keyIsPressed&&
-      keyCode === LEFT_ARROW){
-        this.vel.x = -3;
-        this.vel.y = 0;
-        }
-    if(keyIsPressed&&
-      keyCode === UP_ARROW){
-        this.vel.x = 0;
-        this.vel.y = -3;
-      }
-    if(keyIsPressed&&
-      keyCode === DOWN_ARROW){
-        this.vel.x = 0;
-        this.vel.y = 3;
-        }
-    //this.loc.add(this.vel);
+    rect(this.loc.x * this.w, this.loc.y * this.w, this.w, this.w);
+    for (var i = 0; i < this.body.length; i++){
+      rect(this.body[i].loc.x * this.w, this.body[i].loc.y * this.w, this.w, this.w);
+    }
   }
 }
+
+//  update(){
+
+//}
--- Lab 1031 Snake Game/sketch.js
+++ Lab 1031 Snake Game/sketch.js
@@ -1,16 +1,14 @@
 // Duncan Vaughan
 // 10/31/19
 
-var squares = [];
 var segments = [];
-var food = 0;
-hitFood = 'no';
+
 var score = 0;
 function setup() {
   // put setup code here
   var cnv = createCanvas(800,800);
   cnv.position((windowWidth-width)/2, 30);
-  background(20,20,20);//background color
+  background(255,255,255);//background color
   loadSnake();
 }
 
@@ -18,14 +16,10 @@ function draw(){
   runSnake();
 }
 function loadSnake(){
-  segments[0] = new Snake(0,0,50,50,0);
+  segments[0] = new Snake(100, 100, 50, 50, color(255, 0, 0));
 }
 function runSnake(){
-  for (var i=0; i<segments.length; i++){
+  for (var i = 0; i < segments.length; i++){
     segments[i].run();
-    if (hitFood==='yes'){
-      segments[i+1]= new Snake(segments[i].loc.x-segments[i].w,segments[i].loc.y,50,50,i+1);
-      hitFood='no';
-    }
   }
 }
